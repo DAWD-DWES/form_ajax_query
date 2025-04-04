1a38076ca0c7e35a9998ebf23876f2530d1f45ab
profesor <profesor@dawdistancia.net>
2025-04-04 19:32:47 +0200

    fix

--- index.php
+++ index.php
@@ -4,31 +4,31 @@ define("RETRO_EMAIL_FORMATO", "El correo debe tener un formato correcto");
 define("RETRO_PASS_NO_REPETIDO", "Los passwords introducidos deben de ser iguales");
 define("RETRO_PASS_FORMATO", "El password debe tener una minúscula, mayúscula, digito y caracter espercial");
 
-if (filter_has_var(INPUT_POST, 'enviar')) {
-    $usuario = filter_input(INPUT_POST, 'usuario', FILTER_UNSAFE_RAW);
-    $errorUsuarioFormato = (filter_var($usuario, FILTER_VALIDATE_REGEXP, ["options" => [
-                    "regexp" => "/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`\-]+(\s+[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`\-]+){0,5}$/"]]) === false);
-    $password1 = filter_input(INPUT_POST, 'password1', FILTER_UNSAFE_RAW);
-    $password2 = filter_input(INPUT_POST, 'password2', FILTER_UNSAFE_RAW);
-    $errorPasswordNoRepetido = ($password1 !== $password2);
-    $errorPasswordFormato = (filter_var($password1, FILTER_VALIDATE_REGEXP, ["options" => [
-                    "regexp" => "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}/"]]) === false);
-    $email = filter_input(INPUT_POST, 'email', FILTER_UNSAFE_RAW);
-    $errorEmailFormato = (filter_var($email, FILTER_VALIDATE_REGEXP, ["options" => [
-                    "regexp" => "/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i"]]) === false);
-    $errors = [];
-    if ($errorUsuarioFormato) {
-        $errors['usuario'] = RETRO_NOMBRE_FORMATO;
-    }
-    if ($errorPasswordNoRepetido) {
-        $errors['password2'] = RETRO_PASS_NO_REPETIDO;
-    } else if ($errorPasswordFormato) {
-        $errors['password2'] = RETRO_PASS_FORMATO;
-    }
-    if ($errorEmailFormato) {
-        $errors['email'] = RETRO_EMAIL_FORMATO;
-    }
+if (!empty($_POST)) {
     if (filter_has_var(INPUT_POST, 'enviar')) {
+        $usuario = filter_input(INPUT_POST, 'usuario', FILTER_UNSAFE_RAW);
+        $errorUsuarioFormato = (filter_var($usuario, FILTER_VALIDATE_REGEXP, ["options" => [
+                        "regexp" => "/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`\-]+(\s+[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ'´`\- ]+){0,5}$/"]]) === false);
+        $password1 = filter_input(INPUT_POST, 'password1', FILTER_UNSAFE_RAW);
+        $password2 = filter_input(INPUT_POST, 'password2', FILTER_UNSAFE_RAW);
+        $errorPasswordNoRepetido = ($password1 !== $password2);
+        $errorPasswordFormato = (filter_var($password1, FILTER_VALIDATE_REGEXP, ["options" => [
+                        "regexp" => "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}/"]]) === false);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $errorEmailFormato = (filter_var($email, FILTER_VALIDATE_REGEXP, ["options" => [
+                        "regexp" => "/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i"]]) === false);
+        $errors = [];
+        if ($errorUsuarioFormato) {
+            $errors['usuario'] = RETRO_NOMBRE_FORMATO;
+        }
+        if ($errorPasswordNoRepetido) {
+            $errors['password2'] = RETRO_PASS_NO_REPETIDO;
+        } else if ($errorPasswordFormato) {
+            $errors['password1'] = RETRO_PASS_FORMATO;
+        }
+        if ($errorEmailFormato) {
+            $errors['email'] = RETRO_EMAIL_FORMATO;
+        }
         $response['success'] = empty($errors);
         $response['errors'] = $errors;
         header('Content-type: application/json');
@@ -46,36 +46,37 @@ if (filter_has_var(INPUT_POST, 'enviar')) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap CDN -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Bootstrap Font Icon CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
         <title>Registro</title>
     </head>
     <body class="bg-info">
         <div class="container mt-5">
-            <?php if (isset($procesa)): ?>
+            <?php if ($procesa ?? false): ?>
                 <div class="alert alert-success" id="mensaje" role="alert">
                     Registro realizado con éxito
                 </div>
-            <?php else: ?>
-                <div class="d-flex justify-content-center h-100">
-                    <div class="card w-50">
-                        <div class="card-header">
-                            <h3><i class="bi bi-gear p-2"></i>Registro</h3>
-                        </div>
-                        <div class="card-body">
-                            <form id="registro" name="registro" action="index.php" method="POST" novalidate>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-person"></i></span>
-                                    <input type="text" class="form-control"  placeholder="usuario" 
-                                           id="usuario" name="usuario" value="<?= $usuario ?? '' ?>" autofocus>
-                                    <div class="invalid-feedback">
-                                    </div>
+            <?php endif ?>
+            <div class="d-flex justify-content-center h-100">
+                <div class="card w-50">
+                    <div class="card-header">
+                        <h3><i class="bi bi-gear p-2"></i>Registro</h3>
+                    </div>
+                    <div class="card-body">
+                        <form id="registro" name="registro" action="index.php" method="POST" novalidate>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-person"></i></span>
+                                <input type="text" class="form-control"  placeholder="usuario" 
+                                       id="usuario" name="usuario" value="<?= $usuario ?? '' ?>" autofocus>
+                                <div class="invalid-feedback">
                                 </div>
-                                <div class="input-group my-2">
-                                    <span class="input-group-text"><i class="bi bi-key"></i></span>
-                                    <input type="password" class="form-control" placeholder="contraseña" id="password1" name="password1" 
-                                           value="<?= $password1 ?? '' ?>">
+                            </div>
+                            <div class="input-group my-2">
+                                <span class="input-group-text"><i class="bi bi-key"></i></span>
+                                <input type="password" class="form-control" placeholder="contraseña" id="password1" name="password1" 
+                                       value="<?= $password1 ?? '' ?>">
+                                <div class="invalid-feedback">
                                 </div>
                                 <div class="input-group my-2">
                                     <span class="input-group-text"><i class="bi bi-key"></i></span>
@@ -94,14 +95,12 @@ if (filter_has_var(INPUT_POST, 'enviar')) {
                                 <div class="text-end">
                                     <input type="submit" value="Registrar" class="btn btn-info" name="enviar">
                                 </div>
-                            </form>
-                        </div>
+                        </form>
                     </div>
                 </div>
-            <?php endif ?>
+            </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin = "anonymous"></script>
         <script src="validar.js"></script>
     </body>
 </html>
--- validar.js
+++ validar.js
@@ -1,43 +1,58 @@
 $(document).ready(function () {
-    $('#registro').submit(validaForm);
-});
-function validaForm(e) {
-    e.preventDefault();
-    var form = this;
+    $('#registro').submit(function (e) {
+        e.preventDefault();
 
-    // Usar el método de jQuery para crear el objeto FormData
-    var data = $(this).serialize();
-    data += '&enviar=' + encodeURIComponent($('input[name="enviar"]').val());
+        var form = this;
+        var data = $(form).serialize();
 
-    // Usar jQuery.ajax() para enviar los datos
-    $.ajax({
-        url: 'index.php',
-        type: 'POST',
-        data: data,
-        dataType: 'json', // Esperamos JSON de vuelta
-        success: function (response) {
-            $(form).removeClass('was-validated');
-            // Ajustar la validación en cada campo específico
-            $.each(form.elements, function () {
-                var input = this;
-                var feedback = $(input).next('.invalid-feedback');
-                if (response.errors[input.name]) {
-                    $(feedback).text(response.errors[input.name]);
-                    $(feedback).show();
-                    $(input).addClass('is-invalid').removeClass('is-valid');
-                } else {
-                    $(input).removeClass('is-invalid').addClass('is-valid');
-                    $(feedback).text('').hide();
-                }
-            });
-            if (response.success) {
-                // Si quieres hacer algo al tener éxito, como redireccionar o mostrar un mensaje
-                // alert('Registro completado con éxito.');
-                form.submit(); // O puedes redireccionar usando window.location.href a otra URL
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error('Error en la red o en el servidor: ' + textStatus + ', ' + errorThrown);
+        // Añadir botón submit pulsado
+        var submitBtn = $('input[name="enviar"]');
+        if (submitBtn.length) {
+            data += '&' + encodeURIComponent(submitBtn.attr('name')) +
+                    '=' + encodeURIComponent(submitBtn.val());
         }
+
+        $.ajax({
+            url: 'index.php',
+            type: 'POST',
+            data: data,
+            dataType: 'json',
+            success: function (response) {
+                $(form).removeClass('was-validated');
+
+                // Limpiar feedback anterior
+                $.each(form.elements, function () {
+                    var input = this;
+                    var $input = $(input);
+                    var feedback = $input.closest('.input-group').find('.invalid-feedback');
+                    $input.removeClass('is-invalid is-valid');
+                    if (feedback.length) {
+                        feedback.text('').hide();
+                    }
+                });
+
+                // Mostrar errores si los hay
+                $.each(response.errors, function (fieldName, message) {
+                    var input = form.elements[fieldName];
+                    if (input) {
+                        var $input = $(input);
+                        var feedback = $input.closest('.input-group').find('.invalid-feedback');
+                        $input.addClass('is-invalid');
+                        if (feedback.length) {
+                            feedback.text(message).show();
+                        }
+                    }
+                });
+
+                if (response.success) {
+                    // Reenviar formulario o redirigir
+                    form.submit();
+                    // O bien: window.location.href = 'success.php';
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Error de red o servidor:', textStatus, errorThrown);
+            }
+        });
     });
-}
+});
